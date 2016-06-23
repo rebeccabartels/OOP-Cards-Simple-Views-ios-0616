5821351dc6173310cbe7ae8c605a81055ab944a6
rebeccabartels <rebeccakbartels@icloud.com>
2016-06-23 13:39:34 -0400

    done?

--- OOPCardsSimpleViews/FISCardSwitcherViewController.h
+++ OOPCardsSimpleViews/FISCardSwitcherViewController.h
@@ -9,6 +9,15 @@
 @property (strong, nonatomic) FISCard *fourOfClubs;
 @property (strong, nonatomic) FISCard *eightOfDiamonds;
 @property (strong, nonatomic) FISCard *tenOfHearts;
+@property (weak, nonatomic) IBOutlet UILabel *topLeftLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomRightLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *centerLabel;
+
+
+
+
+
 
 @end
 
--- OOPCardsSimpleViews/FISCardSwitcherViewController.m
+++ OOPCardsSimpleViews/FISCardSwitcherViewController.m
@@ -21,5 +21,36 @@
     self.tenOfHearts = [[FISCard alloc] initWithSuit:@"♥️"
                                                 rank:@"10"];
 }
+- (IBAction)threeOfSpadesButton:(id)sender {
+    NSString *myString = [NSString stringWithFormat:@"%@%@", self.threeOfSpades.suit, self.threeOfSpades.rank];
+    
+    self.topLeftLabel.text = self.threeOfSpades.cardLabel;
+    self.centerLabel.text = myString;
+    self.bottomRightLabel.text = myString;
+    
+}
+- (IBAction)fourOfClubsButton:(id)sender {
+        NSString *myString1 = [NSString stringWithFormat:@"%@%@", self.fourOfClubs.suit, self.fourOfClubs.rank];
+    self.topLeftLabel.text = self.threeOfSpades.cardLabel;
+    self.centerLabel.text = myString1;
+    self.bottomRightLabel.text = myString1;
+}
+- (IBAction)eightOfDiamondsButton:(id)sender {
+        NSString *myString2 = [NSString stringWithFormat:@"%@%@", self.eightOfDiamonds.suit, self.eightOfDiamonds.rank];
+    self.topLeftLabel.text = self.threeOfSpades.cardLabel;
+    self.centerLabel.text = myString2;
+    self.bottomRightLabel.text = myString2;
+}
+- (IBAction)tenOfHeartsButton:(id)sender {
+        NSString *myString3 = [NSString stringWithFormat:@"%@%@", self.tenOfHearts.suit, self.tenOfHearts.rank];
+    self.topLeftLabel.text = self.threeOfSpades.cardLabel;
+    self.centerLabel.text = myString3;
+    self.bottomRightLabel.text = myString3;
+    
+}
+
+
+
+
 
 @end
